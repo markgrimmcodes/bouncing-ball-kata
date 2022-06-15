6dd8bd711b7e22df137adef813b4355a478df535
Mark Grimm <mark.grimm.coding@gmail.com>
2022-06-15 17:18:04 +0200

    finished, ball is bouncing very nicely

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+let x = 0;
+let y = 0;
+let canvasBase = 200;
+let velocityX = 3;
+let velocityY = 5;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(canvasBase, canvasBase);
 }
 
 function draw() {
   background(220);
+
+  circle(x, y, 30);
+  x += velocityX;
+  y += velocityY;
+
+  if (x <= 0 || x >= 200) {
+    velocityX = -velocityX;
+  }
+  if (y <= 0 || y >= 200) {
+    velocityY = -velocityY;
+  }
 }
